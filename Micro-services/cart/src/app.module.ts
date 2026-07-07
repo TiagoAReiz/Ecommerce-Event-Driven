1516fe9f26f4878972136561b4896b62e1e64df0
Tiago Reiz <tiagoreizzzz@gmail.com>
2026-07-07 20:30:33 -0300

    feat: config prisma

--- Micro-services/cart/src/app.module.ts
+++ Micro-services/cart/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './adapters/out/database/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [],
   providers: [],
 })
