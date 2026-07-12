b77b2d7654e665cdf0617f8879f6f320a44f013d
Tiago Reiz <tiagoreizzzz@gmail.com>
2026-07-12 00:46:24 -0300

    feat: implement JWT authentication and reorganize DTO directory structure

--- Micro-services/review/src/application/mappers/review-mapper.ts
+++ Micro-services/review/src/application/mappers/review-mapper.ts
@@ -1,5 +1,5 @@
 import { Review } from "src/core/entities/review-entity";
-import { ReviewRequest } from "src/adapters/in/dtos/review-request";
+import { ReviewRequest } from "src/adapters/in/controllers/dtos/review-request";
 import { randomUUID } from "crypto";
 
 export class ReviewMapper {
