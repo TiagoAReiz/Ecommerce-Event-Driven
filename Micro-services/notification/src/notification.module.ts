fa5af5f7cb5f85a7f99b5a750cca5d9f8ec1b2d2
Tiago Reiz <tiagoreizzzz@gmail.com>
2026-07-22 17:40:03 -0300

    feat(notification): wire SellerProfile repo and the two new event consumers

--- Micro-services/notification/src/notification.module.ts
+++ Micro-services/notification/src/notification.module.ts
@@ -8,15 +8,19 @@ import { AuthEventsConsumer } from './adapters/in/messaging/auth-events.consumer
 import { OrderEventsConsumer } from './adapters/in/messaging/order-events.consumer';
 import { PaymentEventsConsumer } from './adapters/in/messaging/payment-events.consumer';
 import { ShippingEventsConsumer } from './adapters/in/messaging/shipping-events.consumer';
+import { CatalogEventsConsumer } from './adapters/in/messaging/catalog-events.consumer';
+import { ReviewEventsConsumer } from './adapters/in/messaging/review-events.consumer';
 import { NotificationEventService } from './application/services/notification-event.service';
 import { NotificationQueryService } from './application/services/notification-query.service';
 import { UserContactRepository } from './adapters/out/repositories/user-contact.repository';
 import { NotificationRepository } from './adapters/out/repositories/notification.repository';
+import { SellerProfileRepository } from './adapters/out/repositories/seller-profile.repository';
 import { StubEmailSenderService } from './adapters/out/external/stub-email-sender.service';
 import { NOTIFICATION_EVENT_SERVICE } from './core/interfaces/services/notification-event.service.interface';
 import { NOTIFICATION_QUERY_SERVICE } from './core/interfaces/services/notification-query.service.interface';
 import { USER_CONTACT_REPOSITORY } from './core/interfaces/repositories/user-contact-repository.interface';
 import { NOTIFICATION_REPOSITORY } from './core/interfaces/repositories/notification-repository.interface';
+import { SELLER_PROFILE_REPOSITORY } from './core/interfaces/repositories/seller-profile-repository.interface';
 import { EMAIL_SENDER } from './core/interfaces/external/email-sender.interface';
 
 @Module({
@@ -27,6 +31,7 @@ import { EMAIL_SENDER } from './core/interfaces/external/email-sender.interface'
     { provide: NOTIFICATION_QUERY_SERVICE, useClass: NotificationQueryService },
     { provide: USER_CONTACT_REPOSITORY, useClass: UserContactRepository },
     { provide: NOTIFICATION_REPOSITORY, useClass: NotificationRepository },
+    { provide: SELLER_PROFILE_REPOSITORY, useClass: SellerProfileRepository },
     { provide: EMAIL_SENDER, useClass: StubEmailSenderService },
     { provide: APP_FILTER, useClass: DomainExceptionFilter },
     JwtAuthGuard,
@@ -34,6 +39,8 @@ import { EMAIL_SENDER } from './core/interfaces/external/email-sender.interface'
     OrderEventsConsumer,
     PaymentEventsConsumer,
     ShippingEventsConsumer,
+    CatalogEventsConsumer,
+    ReviewEventsConsumer,
   ],
 })
 export class NotificationModule {}
